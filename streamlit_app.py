7d4188914ee268f717546d8251a50dd232087641
Jorge Giménez García <46479562+JorgeGimenezGarcia@users.noreply.github.com>
2025-01-21 12:37:32 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,6 +20,8 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('SEARC
 # st.stop()
 
 pd_df = my_dataframe.to_pandas()
+st.write("Columns:", pd_df.columns)
+st.stop()
 # st.dataframe(pd_df)
 # st.stop()
 
